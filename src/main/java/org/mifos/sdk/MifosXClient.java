d9dc8d4cb1ca47730760e0ad464def8192555c5c
Mohammed Nafees <nafees.technocool@gmail.com>
2014-12-08 22:52:42 +0530

    MIFOSX-1716 Setup Gradle Project and implement skeleton

---
+++ src/main/java/org/mifos/sdk/MifosXClient.java
@@ -0,0 +1,25 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.mifos.sdk;
+
+/**
+ * Principle client interface for the base authentication workflow
+ */
+public interface MifosXClient {
+
+    /**
+     * Authenticates into the MifosX server by obtaining an authentication key.
+     * If a valid authentication key already exists, this method does nothing.
+     */
+    abstract void login();
+
+    /**
+     * Deletes the authnetication key. To be able to carry further operations,
+     * you must call {@link #login()} to obtain a new authentication key.
+     */
+    abstract void logout();
+
+}
